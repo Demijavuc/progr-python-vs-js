ff42c87e3e4a90ce8c2272ac808cd36f4c479d7c
Kalvis Kincis <kalvis.kincis@r6vsk.lv>
2021-01-05 16:44:04 +0200

    JS un Python vienkāršas izvades piemēri

---
+++ 23-JS-demo-1.js
@@ -0,0 +1,9 @@
+console.log("Labrīt visiem!");
+console.log(33+22);
+
+let sk1 = 10;
+let sk2 = 20;
+console.log(sk1+sk2);
+
+let gadalaiks = "ziema";
+console.log(`Ārā ir ${gadalaiks}`);
---
+++ 23-python-demo-1.py
@@ -0,0 +1,9 @@
+print("Labrīt visiem!")
+print(33+22)
+
+sk1 = 10
+sk2 = 20
+print(sk1+sk2)
+
+gadalaiks = "ziema"
+print(f"Ārā ir {gadalaiks}")
